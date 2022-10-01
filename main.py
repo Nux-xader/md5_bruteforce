5a9244407b2d5f6f7458a2c0bf70dd6e10f2c524
Satria Rahmat <sumgmng@gmail.com>
2022-10-02 00:03:53 +0700

    initial

---
+++ main.py
@@ -0,0 +1,25 @@
+import hashlib, sys, os
+
+
+clr = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
+def banner():
+    print(f"""
+ █▀▄▀█ █▀▄ █▀   █░█ ▄▀█ █▀ █░█
+ █░▀░█ █▄▀ ▄█   █▀█ █▀█ ▄█ █▀█
+ 
+ █▄▄ █▀▀
+ █▄█ █▀░  just for fun
+ _______________________________
+""")
+
+def load_wordlist():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
